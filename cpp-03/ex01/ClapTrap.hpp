f10da274d65b6be5a8867bcd4b82e823726121fc
palmicid <palmicid@gmail.com>
2023-08-05 17:43:03 +0700

    FIX private to protectd

--- cpp-03/ex01/ClapTrap.hpp
+++ cpp-03/ex01/ClapTrap.hpp
@@ -26,7 +26,7 @@ class ClapTrap
 		void	attack(const std::string& target);
 		void	takeDamage(unsigned int amount);
 		void	beRepaired(unsigned int amount);
-	private:
+	protected:
 		std::string		_name;
 		int				_hitPoints;
 		int				_energyPoints;
